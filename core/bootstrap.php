9fd643fb14f9fe564edd073f587cd6334fe094b9
LAPTOP-JEREMY\Jeremy <krummenacherjeremy@gmail.com>
2022-05-10 10:35:10 +0200

    model added

--- core/bootstrap.php
+++ core/bootstrap.php
@@ -1,6 +1,9 @@
 <?php
 define ('ROOT_URL', $_SERVER["REQUEST_SCHEME"] . '://' . $_SERVER['SERVER_NAME'] . dirname($_SERVER['SCRIPT_NAME']));
 
+require 'app/Models/Tasks.php';
+
+require 'core/database.php';
 require 'core/helpers.php';
 require 'core/Router.php';
 
